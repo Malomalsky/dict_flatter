0a6396af6220e53311c712386b70f89842f67ec9
malomalsky <apgreydd@gmail.com>
2020-11-17 18:31:31 +0300

    Added docstring

--- function.py
+++ function.py
@@ -1,17 +1,27 @@
-from collections import MutableMapping # from collection.abc если python >= 3.8
+from collections import MutableMapping  # from collection.abc если python >= 3.8
 
 
+def flatten(dct, pkey='', sep='.') -> dict:
+    """
+    Возвращает сглаженный список.
 
-def flatten(dct, p_key ='', sep='.'): 
-    if isinstance(dct, MutableMapping): 
+    Аргументы:\n
+        dct (MutableMapping): Входной словарь
+        pkey (str): Родительский ключ, дефолт - пустой для корректной работы
+        sep (str): Разделитель. ('.' по дефолту)
+
+    Результат:\n
+        dict: Сглаженный словарь
+        TypeError: Если на вход функции передан не словарь.
+    """
+    if isinstance(dct, MutableMapping):
         pairs = []
-        for key, value in dct.items(): 
-            new_key = p_key + sep + key if p_key else key
-            if isinstance(value, MutableMapping): 
-                pairs.extend(flatten(value, p_key = new_key).items())
-            else: 
+        for key, value in dct.items():
+            new_key = pkey + sep + key if pkey else key
+            if isinstance(value, MutableMapping):
+                pairs.extend(flatten(value, pkey=new_key).items())
+            else:
                 pairs.append((new_key, value))
         return dict(pairs)
     else:
         raise TypeError('Неверный формат словаря.')
-
